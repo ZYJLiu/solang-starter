38aa476a0946ec48ec012b8d566ec944b8c06b71
John <johnzhengyuliu@gmail.com>
2023-07-10 23:11:03 -0500

    cpi

---
+++ 03_CPI/solidity/cpi.sol
@@ -0,0 +1,44 @@
+import 'solana';
+
+@program_id("bXHdKxE4Ti8gsQUSSWvhodPpgjcQFHtdnYx2Ab1S2S2")
+contract flipper {
+    bool private value = true;
+
+    @payer(payer)
+    constructor(address payer) {
+        print("Hello, World!");
+    }
+
+    /// A message that can be called on instantiated contracts.
+    /// This one flips the value of the stored `bool` from `true`
+    /// to `false` and vice versa.
+    function flip() public {
+            value = !value;
+    }
+
+    /// Simply returns the current value of our `bool`.
+    function get() public view returns (bool) {
+            return value;
+    }
+}
+
+// Interface to the flipper program.
+flipperInterface constant flipperProgram = flipperInterface(address'bXHdKxE4Ti8gsQUSSWvhodPpgjcQFHtdnYx2Ab1S2S2');
+interface flipperInterface {
+    function flip() external;
+}
+
+@program_id("JCEZ3WFA6jCP35ywaLZaqn8dSs1p3JQXJFqKwGJffCVZ")
+contract cpi {
+
+    @payer(payer)
+    constructor(address payer) {}
+
+    function flipCpi(address dataAccount) public {
+        AccountMeta[1] metas = [
+            AccountMeta({pubkey: dataAccount, is_writable: true, is_signer: false})
+        ];
+
+        flipperProgram.flip{accounts: metas}();
+    }
+}
---
+++ 03_CPI/tests/cpi.ts
@@ -0,0 +1,80 @@
+import * as anchor from "@coral-xyz/anchor"
+import { Program } from "@coral-xyz/anchor"
+import { Cpi } from "../target/types/cpi"
+import { Flipper } from "../target/types/flipper"
+
+describe("cpi", () => {
+  // Configure the client to use the local cluster.
+  const provider = anchor.AnchorProvider.env()
+  anchor.setProvider(provider)
+
+  const dataAccount = anchor.web3.Keypair.generate()
+  const dataAccount2 = anchor.web3.Keypair.generate()
+  const wallet = provider.wallet
+
+  const flipperProgram = anchor.workspace.Flipper as Program<Flipper>
+  const cpiProgram = anchor.workspace.Cpi as Program<Cpi>
+
+  it("Is initialized!", async () => {
+    // Add your test here.
+    const tx = await flipperProgram.methods
+      .new(wallet.publicKey)
+      .accounts({ dataAccount: dataAccount.publicKey })
+      .signers([dataAccount])
+      .rpc()
+    console.log("Your transaction signature", tx)
+
+    const val1 = await flipperProgram.methods
+      .get()
+      .accounts({ dataAccount: dataAccount.publicKey })
+      .view()
+
+    console.log("state", val1)
+
+    await flipperProgram.methods
+      .flip()
+      .accounts({ dataAccount: dataAccount.publicKey })
+      .rpc()
+
+    const val2 = await flipperProgram.methods
+      .get()
+      .accounts({ dataAccount: dataAccount.publicKey })
+      .view()
+
+    console.log("state", val2)
+  })
+
+  it("Flip CPI", async () => {
+    // Add your test here.
+    const tx = await cpiProgram.methods
+      .new(wallet.publicKey)
+      .accounts({ dataAccount: dataAccount2.publicKey })
+      .signers([dataAccount2])
+      .rpc()
+    console.log("Your transaction signature", tx)
+
+    await cpiProgram.methods
+      .flipCpi(dataAccount.publicKey)
+      .accounts({ dataAccount: dataAccount2.publicKey })
+      .remainingAccounts([
+        {
+          pubkey: flipperProgram.programId,
+          isWritable: false,
+          isSigner: false,
+        },
+        {
+          pubkey: dataAccount.publicKey,
+          isWritable: true,
+          isSigner: false,
+        },
+      ])
+      .rpc()
+
+    const val = await flipperProgram.methods
+      .get()
+      .accounts({ dataAccount: dataAccount.publicKey })
+      .view()
+
+    console.log("state", val)
+  })
+})
